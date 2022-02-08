8653e527546052989cd286d96a63755d8e46b10e
Michael Yogar <michaelyogar878@gmail.com>
2022-02-08 17:15:34 -0500

    feat: button listener to switch activity

--- app/src/main/java/com/example/movietrivia/MainActivity.kt
+++ app/src/main/java/com/example/movietrivia/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.movietrivia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        val button: Button = findViewById(R.id.btn_start)
+        val editText: EditText = findViewById(R.id.et_name)
+        button.setOnClickListener { view ->
+            if (editText.text.toString().isEmpty()) {
+                Toast.makeText(this, "Enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, TriviaQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/movietrivia/TriviaQuestionsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.movietrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TriviaQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trivia_questions)
+    }
+}
