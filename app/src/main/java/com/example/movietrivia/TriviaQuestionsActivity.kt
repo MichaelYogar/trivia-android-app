144956e02702cf211a6ccba752e6200d54f51e0c
Michael Yogar <michaelyogar878@gmail.com>
2022-02-08 22:24:01 -0500

    feat: added http get request with Volley

--- app/src/main/java/com/example/movietrivia/TriviaQuestionsActivity.kt
+++ app/src/main/java/com/example/movietrivia/TriviaQuestionsActivity.kt
@@ -1,11 +1,32 @@
 package com.example.movietrivia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class TriviaQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia_questions)
+        apiCall()
     }
+
+    private fun apiCall() {
+        val url = "https://cat-fact.herokuapp.com/facts"
+        val queue = Volley.newRequestQueue(this)
+
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null,
+            {
+                Log.d("TriviaQuestionsActivity", it.getJSONObject(0).toString())
+            },
+            {
+                Log.d("TriviaQuestionsActivity", it.message.toString())
+            }
+        )
+        queue.add(jsonArrayRequest)
+    }
+
 }
