a03b8c2af1629dda3ee472fe56cf5c56fe9624fc
Michael Yogar <michaelyogar878@gmail.com>
2022-02-09 14:50:26 -0500

    refactor: Kotlin coroutine for api call

---
+++ app/src/main/java/com/example/movietrivia/Question.kt
@@ -0,0 +1,5 @@
+package com.example.movietrivia
+
+data class Question(
+    val question: String,
+)
--- app/src/main/java/com/example/movietrivia/TriviaData.kt
+++ app/src/main/java/com/example/movietrivia/TriviaData.kt
@@ -3,23 +3,31 @@ package com.example.movietrivia
 import android.content.Context
 import android.util.Log
 import com.android.volley.Request
-import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONArray
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+private const val URL_PATH = "https://opentdb.com/api.php?amount=10&category=11&type=multiple"
 
 class TriviaData(val ctx: Context) {
-
-    fun getInfo() {
-        val url = "https://cat-fact.herokuapp.com/facts"
-
-        val jsonArrayRequest = JsonArrayRequest(
-            Request.Method.GET, url, null,
+    suspend fun getTriviaQuestions() = suspendCoroutine<Question>{ cont ->
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, URL_PATH, null,
             {
-                Log.d("TriviaQuestionsActivity", "success")
-                Log.d("TriviaQuestionsActivity", it.getJSONObject(0).toString())
+                val results: JSONArray = it.getJSONArray("results")
+
+                for (i in 0 until 1) {
+                    val item = results.getJSONObject(i)
+                    Log.d("TriviaQuestionsActivity", item.toString())
+                    cont.resume(Question(question = item.getString("question")))
+                }
             },
             {
                 Log.d("TriviaQuestionsActivity", it.message.toString())
             }
         )
-        TriviaManager.getInstance(ctx).addToRequestQueue(jsonArrayRequest)
+        TriviaManager.getInstance(ctx).addToRequestQueue(jsonObjectRequest)
     }
+
 }
