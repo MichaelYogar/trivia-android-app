e67c2bf8a2a2439b479819ba7f75291fcdee3605
Michael Yogar <michaelyogar878@gmail.com>
2022-02-10 11:47:53 -0500

    feat: Store api data into list of questions and display on screen

--- app/src/main/java/com/example/movietrivia/TriviaData.kt
+++ app/src/main/java/com/example/movietrivia/TriviaData.kt
@@ -2,6 +2,7 @@ package com.example.movietrivia
 
 import android.content.Context
 import android.util.Log
+import androidx.core.text.HtmlCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONArray
@@ -11,17 +12,22 @@ import kotlin.coroutines.suspendCoroutine
 private const val URL_PATH = "https://opentdb.com/api.php?amount=10&category=11&type=multiple"
 
 class TriviaData(val ctx: Context) {
-    suspend fun getTriviaQuestions() = suspendCoroutine<Question>{ cont ->
+    suspend fun getTriviaQuestions() = suspendCoroutine<List<Question>> { cont ->
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, URL_PATH, null,
             {
                 val results: JSONArray = it.getJSONArray("results")
-
-                for (i in 0 until 1) {
+                val questions: MutableList<Question> = mutableListOf()
+                for (i in 0 until results.length()) {
                     val item = results.getJSONObject(i)
-                    Log.d("TriviaQuestionsActivity", item.toString())
-                    cont.resume(Question(question = item.getString("question")))
+                    val questionString : String = HtmlCompat.fromHtml(
+                        item.getString("question"),
+                        HtmlCompat.FROM_HTML_MODE_LEGACY
+                    ).toString()
+
+                    questions.add(Question(question = questionString))
                 }
+                cont.resume(questions.toList())
             },
             {
                 Log.d("TriviaQuestionsActivity", it.message.toString())
--- app/src/main/java/com/example/movietrivia/TriviaQuestionsActivity.kt
+++ app/src/main/java/com/example/movietrivia/TriviaQuestionsActivity.kt
@@ -2,20 +2,45 @@ package com.example.movietrivia
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
 class TriviaQuestionsActivity : AppCompatActivity() {
+
+    private lateinit var option1: TextView
+    private lateinit var option2: TextView
+    private lateinit var option3: TextView
+    private lateinit var option4: TextView
+    private lateinit var tvProgress: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var result: List<Question>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia_questions)
+        option1 = findViewById(R.id.tv_option_one)
+        option2 = findViewById(R.id.tv_option_two)
+        option3 = findViewById(R.id.tv_option_three)
+        option4 = findViewById(R.id.tv_option_four)
+        tvProgress = findViewById(R.id.tv_progress)
+        progressBar = findViewById(R.id.progress_bar)
         lifecycleScope.launch {
-            val result = apiCall()
+            result = apiCall()
+            Log.d("TriviaQuestionsActivity", result.size.toString())
+            option1.text = result[0].question
+            option2.text = result[1].question
+            option3.text = result[2].question
+            option4.text = result[3].question
+            val currentPosition = 1
+            tvProgress.text = "$currentPosition" + "/" + progressBar.max
+            progressBar.progress = 1
         }
     }
 
-    private suspend fun apiCall() {
-        TriviaData(this).getTriviaQuestions()
+    private suspend fun apiCall(): List<Question> {
+        return TriviaData(this).getTriviaQuestions()
     }
 }
