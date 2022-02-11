cd84a7eaaa081ca281a3f55afdece5d422034c99
Michael Yogar <michaelyogar878@gmail.com>
2022-02-11 12:58:56 -0500

    refactor: Updated results screen and different colors

--- app/src/main/java/com/example/movietrivia/ResultActivity.kt
+++ app/src/main/java/com/example/movietrivia/ResultActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var tvScore: TextView
-    private lateinit var tvUsername: TextView
+    private lateinit var tvTitle: TextView
     private lateinit var btnPlayAgain: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +17,10 @@ class ResultActivity : AppCompatActivity() {
         val username = intent.getStringExtra("username")
         val score = intent.getStringExtra("score")
         tvScore = findViewById(R.id.tv_score)
-        tvUsername = findViewById(R.id.tv_username)
+        tvTitle = findViewById(R.id.tv_result_title)
         btnPlayAgain = findViewById(R.id.btn_play_again)
-        tvUsername.text = username
-        tvScore.text = score
+        tvScore.text = "You got $score question(s) correct!"
+        tvTitle.text = "${tvTitle.text} ${username}!"
         btnPlayAgain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
